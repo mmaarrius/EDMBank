31676386be151f23f0981c3bdd441e896658e48c
OElizaMaria <oaneaelizamaria@gmail.com>
2025-11-02 11:23:14 +0200

    Added Data Base with 50 randomly generated users

---
+++ DataBase/DataBase.py
@@ -0,0 +1,48 @@
+import os
+import random
+import firebase_admin
+from firebase_admin import credentials, firestore
+from faker import Faker
+from datetime import datetime
+
+key_path = os.path.join(os.path.dirname(__file__),"edmbank-7fd19-firebase-adminsdk-fbsvc-97c017e5cd.json")
+
+cred = credentials.Certificate(key_path)
+
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+fake = Faker()
+
+# Cream 50 de utilizatori random
+# contul fiecarui utilizator are:
+# nume  parola  nr card(16cifre) CVV data de expirare
+
+for i in range(50):
+    UserName = fake.name()
+
+    CardDigit = random.randrange(10**15,10**16-1)
+
+    CVV = random.randrange(10**2,10**3)
+
+    current_year = datetime.now().year
+    year_first = current_year + 1
+    year_last = current_year + 20
+    rand_year = random.randrange(year_first, year_last)
+    rand_month = random.randrange(1,12)
+    if rand_month < 10:
+        expiry_date = "0" + str(rand_month) + "/" + str(rand_year%100) 
+    else:
+        expiry_date = str(rand_month) + "/" + str(rand_year%100)
+
+    Password = random.randrange(10**6,10**7)
+
+    user_data = {
+        "Name" : UserName,
+        "Password" : Password, # trb schimbat sa faca hash
+        "Card_Number" : CardDigit,
+        "CVV" : CVV,
+        "Expiry_date" : expiry_date
+    }
+    db.collection("Users").add(user_data)
