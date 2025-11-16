b57535c012f2c7b4569c8d5638ccb5bd2819d479
OElizaMaria <oaneaelizamaria@gmail.com>
2025-11-16 14:23:29 +0200

    Added DataBase class, test.py should be removed when finalizing project

--- DataBase/DataBase.py
+++ DataBase/DataBase.py
@@ -1,48 +1,107 @@
+import bcrypt
 import os
+import bcrypt
 import random
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials
+from firebase_admin import firestore
 from faker import Faker
 from datetime import datetime
 
-key_path = os.path.join(os.path.dirname(__file__),"edmbank-7fd19-firebase-adminsdk-fbsvc-97c017e5cd.json")
+class Database:
+    def __init__(self):
+        self.initializareBazaDate()
 
-cred = credentials.Certificate(key_path)
+    def initializareBazaDate(self):
+        key_path = os.path.join(os.path.dirname(__file__),"edmbank-7fd19-firebase-adminsdk-fbsvc-97c017e5cd.json")
+        if not firebase_admin._apps:
+            cred = credentials.Certificate(key_path)
+            firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
 
-firebase_admin.initialize_app(cred)
+    def adaugaUtilizator(self, username, Password, email, sum):
+        CardDigit = random.randrange(10**15,10**16-1)
+        CVV = random.randrange(10**2,10**3)
+        current_year = datetime.now().year
+        year_first = current_year + 1
+        year_last = current_year + 20
+        rand_year = random.randrange(year_first, year_last)
+        rand_month = random.randrange(1,12)
+        if rand_month < 10:
+            expiry_date = "0" + str(rand_month) + "/" + str(rand_year%100) 
+        else:
+            expiry_date = str(rand_month) + "/" + str(rand_year%100)
+        Password_bytes = str(Password).encode()
+        salt = bcrypt.gensalt()
+        Password = bcrypt.hashpw(Password_bytes, salt)
+        Password = Password.decode()
 
-db = firestore.client()
+        user_data = {
+            "Name" : username,
+            "Password_hash" : Password,
+            "Card_Number" : CardDigit,
+            "CVV" : CVV,
+            "Expiry_date" : expiry_date,
+            "Sold" : sum,
+            "Email" : email,
+            "Istoric" : []
+        }
+        self.db.collection("Users").document(username).set(user_data)
 
-fake = Faker()
+    def checkUserLogin(self, username, Password):
+        doc_ref = self.db.collection("Users").document(username)
+        doc = doc_ref.get()
 
-# Cream 50 de utilizatori random
-# contul fiecarui utilizator are:
-# nume  parola  nr card(16cifre) CVV data de expirare
+        if not doc.exists:
+            return False
+        stored_hash = doc.to_dict().get("Password_hash")
+        if bcrypt.checkpw(Password.encode(), stored_hash.encode()):
+            return True
+        else:
+            return False  
 
-for i in range(50):
-    UserName = fake.name()
+    def adaugaIstoric(self, username, amount, receiver):
+        doc_ref = self.db.collection("Users").document(username)
+        doc = doc_ref.get()
 
-    CardDigit = random.randrange(10**15,10**16-1)
+        entry = "You have sent " + str(amount) + " to " + receiver+ "."
+        doc_ref.update({"Istoric" : firestore.ArrayUnion([entry])})
 
-    CVV = random.randrange(10**2,10**3)
+    def extrageDate(self, username, date):
+        doc_ref = self.db.collection("Users").document(username)
+        doc = doc_ref.get()
+        #date este o valoare care indica
+        #field ul care urmeaza sa fie returnat
+        # card - nr =1, cvv = 2, expiry date = 3, sold = 4, email =5, istoric = 6
+        if date == 1:
+            return doc.get("Card_Number")
+        elif date == 2:
+            return doc.get("CVV")
+        elif date == 3:
+            return doc.get("Expiry_date")
+        elif date == 4:
+            return doc.get("Sold")
+        elif date == 5:
+            return doc.get("Email")
+        elif date == 6:
+            return doc.get("Istoric")
+        else:
+            return None
+        
 
-    current_year = datetime.now().year
-    year_first = current_year + 1
-    year_last = current_year + 20
-    rand_year = random.randrange(year_first, year_last)
-    rand_month = random.randrange(1,12)
-    if rand_month < 10:
-        expiry_date = "0" + str(rand_month) + "/" + str(rand_year%100) 
-    else:
-        expiry_date = str(rand_month) + "/" + str(rand_year%100)
+    def stergeUtilizator(self, username):
+        doc_ref = self.db.collection("Users").document(username)
+        doc = doc_ref.get()
+        doc_ref.delete()
 
-    Password = random.randrange(10**6,10**7)
+    def modifica(self, username, varianta, modificator):
+        doc_ref = self.db.collection("Users").document(username)
+        doc = doc_ref.get()
+        if varianta == 1: # vrea sa modifice username
+            data = doc.to_dict()
+            data["Name"] = modificator
+            self.db.collection("Users").document(modificator).set(data)
+            doc_ref.delete()
 
-    user_data = {
-        "Name" : UserName,
-        "Password" : Password, # trb schimbat sa faca hash
-        "Card_Number" : CardDigit,
-        "CVV" : CVV,
-        "Expiry_date" : expiry_date
-    }
-    db.collection("Users").add(user_data)
+        if varianta == 2: # vrea sa modifice email
+            doc_ref.update({"Email" : modificator})
